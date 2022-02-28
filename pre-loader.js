f0b7b8cbfad8d8cb76718f4189c88b05f9233823
codewithshamim <shamimislamonline@gmail.com>
2022-02-28 14:02:10 +0600

    preloader & spinner update

--- pre-loader.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-        const loader = document.getElementById('spinner');
-        loader.style.display = 'none';
-    }, 2000);
-}
--- sports-db.js
+++ sports-db.js
@@ -1,15 +1,44 @@
 const errorMessage = document.getElementById('error-msg');
 errorMessage.style.display = 'none';
+// spinner 
 const spinner = document.getElementById('spinner');
-spinner.style.display = 'none'
+spinner.style.display = 'none';
 
+
+// spinner-------------------------------
+const addSpinner = (text) => {
+    spinner.style.display = text;
+}
+
+// remove parent item-----------
+const removeParent = () => {
+    const rowParent = document.getElementById('row-parent');
+    rowParent.textContent = "";
+}
+
+// pre-loader-----------------------
+window.onload = function() {
+    setTimeout(function() {
+        const preLoader = document.getElementById('pre-loader');
+        preLoader.style.display = 'none';
+
+    }, 2000);
+
+
+}
 
 
 const search = () => {
     const inputField = document.getElementById('input-field');
     const inputFieldValue = inputField.value;
     inputField.value = "";
-    spinner.style.display = 'block'
+
+    // add spinner 
+    addSpinner('block');
+    // remove parent 
+    removeParent()
+
+
     if (inputFieldValue == '') {
         alert('PLease!! insert a value!')
         spinner.style.display = 'none'
@@ -28,31 +57,17 @@ const search = () => {
     const displayPlayer = players => {
         const rowParent = document.getElementById('row-parent');
         errorMessage.style.display = 'none';
-        rowParent.textContent = "";
+        // remove parent 
+        removeParent()
+
         for (const player of players.player) {
             const div = document.createElement('div');
             div.classList.add('col');
-            if (player.strThumb == undefined) {
-                // const img = document.createElement('img');
-                // img.src = `./pic.png`
-                // div.appendChild(img)
-                div.innerHTML = `
-                <div class="card">
-                    <img src="./images/pic.jpg" class="rounded card-img-top" alt="..." />
-                    <div class="card-body text-center">
-                    <h6>Date: ${player.dateBorn}</h6>
-                        <h5 class="card-title">${player.strPlayer}</h5>
-                        <p class="card-text">
-                        where he won the Belgian Ebony Shoe. This led to a move to Dutch side Ajax in 2001, from where he joined Celta Vigo on loan in 2003. His next destination was Marseille in France and he left them for Italian side Roma in 2004. He joined English side Tottenham Hotspur on an 18-month loan in 2005 and eventually joined the club permanently in 2006. He left the club in 2007 to join
-                        </p>
-                    </div>
-                </div>
-                 `
-            } else {
-                div.innerHTML = `
+
+            div.innerHTML = `
             <div class="card">
-                <img src="${player.strThumb}" class="rounded card-img-top" alt="..." />
-                <div class="card-body text-center">
+                <img src="${player.strThumb? player.strThumb:'./images/pic.jpg'}" class="rounded card-img-top" alt="..." />
+            <div class="card-body text-center">
                 <h6>Date: ${player.dateBorn}</h6>
                     <h5 class="card-title">${player.strPlayer}</h5>
                     <p class="card-text">
@@ -60,14 +75,14 @@ const search = () => {
                     </p>
                 </div>
             </div>
-             `
-            }
-
+    `
 
             rowParent.appendChild(div);
 
         }
-        spinner.style.display = 'none'
+
+        // add spinner 
+        addSpinner('none')
 
     }
 
